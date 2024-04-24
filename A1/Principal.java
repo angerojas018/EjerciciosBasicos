782528a01cc103d225db98d13586cf76cda34db0
angerojas018 <159502863+angerojas018@users.noreply.github.com>
2024-04-24 11:57:20 -0500

    Add files via upload

---
+++ A1/Principal.java
@@ -0,0 +1,33 @@
+package A1;
+
+
+public class Principal {
+    
+    public static void main(String[] args) {
+    
+        Papel p = new Papel();
+        Papel p2 = new Papel();
+        Papel p3 = new Papel();
+        
+        p.Color = "Blanco";
+        p.Grosor = 1;
+        
+        p2.Color = "Amarillo";
+        p2.Grosor = 3;
+        
+        p3.Color = "Negro";
+        p3.Grosor = 5;        
+        
+        p.mostrarDatos();
+        p2.mostrarDatos();
+        p3.mostrarDatos();
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    }
